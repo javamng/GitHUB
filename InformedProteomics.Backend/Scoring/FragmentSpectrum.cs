8c1b1ba0709213875343439d5158c694c108443c
Kyowon Jeong <kyowonjeong@gmail.com>
2013-02-11 22:20:01 +0000

    Scoring update Scoring test added Sequence.cs constructors updated

--- InformedProteomics.Backend/Scoring/FragmentSpectrum.cs
+++ InformedProteomics.Backend/Scoring/FragmentSpectrum.cs
@@ -1,6 +1,19 @@
-﻿namespace InformedProteomics.Backend.Scoring
+﻿using System.Collections.Generic;
+
+namespace InformedProteomics.Backend.Scoring
 {
-    internal class FragmentSpectrum
+    public class FragmentSpectrum : Dictionary<IonType, double>
     {
+        public double GetIntensity(IonType ion)
+        {
+            return !ContainsKey(ion) ? 0 : this[ion];
+        }
+
+        public void AddIntensity(IonType ion, double intensity)
+        {
+            var prevIntensity = 0.0;
+            if (ContainsKey(ion)) prevIntensity = this[ion];
+            this[ion] = prevIntensity + intensity;
+        }
     }
 }
